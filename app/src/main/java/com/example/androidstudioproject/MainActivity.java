9669f8c7bbbb2070a6acf9cf49627e7b9d4d75a1
StatusXd <Alex_003rus@outlook.com>
2023-02-16 21:55:33 +0300

    Program update commit

--- app/src/main/java/com/example/androidstudioproject/MainActivity.java
+++ app/src/main/java/com/example/androidstudioproject/MainActivity.java
@@ -14,38 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(getApplicationContext(), "onCreate",Toast.LENGTH_LONG);
+        toast.show();
         Log.d(TAG, "onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Toast toast = Toast.makeText(getApplicationContext(), "onStart",Toast.LENGTH_LONG);
+        toast.show();
         Log.d(TAG, "onStart");
     }
     @Override
     protected void onStop() {
         super.onStop();
+        Toast toast = Toast.makeText(getApplicationContext(), "onStop",Toast.LENGTH_LONG);
+        toast.show();
         Log.d(TAG, "onStop");
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Toast toast = Toast.makeText(getApplicationContext(), "onDestroy",Toast.LENGTH_LONG);
+        toast.show();
         Log.d(TAG, "onDestroy");
     }
     @Override
     protected void onPause() {
         super.onPause();
+        Toast toast = Toast.makeText(getApplicationContext(), "onPause",Toast.LENGTH_LONG);
+        toast.show();
         Log.d(TAG, "onPause");
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume");
-    }
-
-    public  void onClick(View view){
-        Toast toast = Toast.makeText(this, "Это Тост",Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(getApplicationContext(), "onResume",Toast.LENGTH_LONG);
         toast.show();
-        Log.d(TAG, "onClick");
+        Log.d(TAG, "onResume");
     }
 }
