0ac73ecacc38fee2c8dc9c0e9f524d476096f64a
StatusXd <Alex_003rus@outlook.com>
2023-02-16 15:33:37 +0300

    LogCat commit

--- app/src/main/java/com/example/androidstudioproject/MainActivity.java
+++ app/src/main/java/com/example/androidstudioproject/MainActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "ACTIVITY_STATE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
     }
 
     public  void onClick(View view){
         Toast toast = Toast.makeText(this, "Это Тост",Toast.LENGTH_LONG);
         toast.show();
+        Log.d(TAG, "onClick");
     }
 }
