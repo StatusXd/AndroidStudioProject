ccdab3e92ac24c00343aafff39d9e0893d0197cd
StatusXd <Alex_003rus@outlook.com>
2023-02-16 21:06:09 +0300

    Life cycle update commit

--- app/src/main/java/com/example/androidstudioproject/MainActivity.java
+++ app/src/main/java/com/example/androidstudioproject/MainActivity.java
@@ -17,6 +17,32 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
     public  void onClick(View view){
         Toast toast = Toast.makeText(this, "Это Тост",Toast.LENGTH_LONG);
         toast.show();
