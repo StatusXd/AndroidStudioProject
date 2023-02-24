63f49a4cd1a6b094f9db5a8ab4cfa6034d008a97
StatusXd <Alex_003rus@outlook.com>
2023-02-24 13:47:45 +0300

    Program update commit

--- app/src/main/java/com/example/androidstudioproject/MainActivity.java
+++ app/src/main/java/com/example/androidstudioproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidstudioproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast toast = Toast.makeText(getApplicationContext(), "onCreate",Toast.LENGTH_LONG);
         toast.show();
-        Log.d(TAG, "onCreate");
     }
 
     @Override
@@ -24,34 +25,34 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Toast toast = Toast.makeText(getApplicationContext(), "onStart",Toast.LENGTH_LONG);
         toast.show();
-        Log.d(TAG, "onStart");
     }
     @Override
     protected void onStop() {
         super.onStop();
         Toast toast = Toast.makeText(getApplicationContext(), "onStop",Toast.LENGTH_LONG);
         toast.show();
-        Log.d(TAG, "onStop");
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Toast toast = Toast.makeText(getApplicationContext(), "onDestroy",Toast.LENGTH_LONG);
         toast.show();
-        Log.d(TAG, "onDestroy");
     }
     @Override
     protected void onPause() {
         super.onPause();
         Toast toast = Toast.makeText(getApplicationContext(), "onPause",Toast.LENGTH_LONG);
         toast.show();
-        Log.d(TAG, "onPause");
     }
     @Override
     protected void onResume() {
         super.onResume();
         Toast toast = Toast.makeText(getApplicationContext(), "onResume",Toast.LENGTH_LONG);
         toast.show();
-        Log.d(TAG, "onResume");
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
