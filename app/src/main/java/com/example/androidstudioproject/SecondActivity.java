b264bb422c68353d3af9527cfdc232e9038e78b5
StatusXd <Alex_003rus@outlook.com>
2023-03-25 01:15:58 +0300

    Global Update

--- app/src/main/java/com/example/androidstudioproject/SecondActivity.java
+++ app/src/main/java/com/example/androidstudioproject/SecondActivity.java
@@ -1,14 +1,26 @@
 package com.example.androidstudioproject;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout);
+        setContentView(R.layout.activity_second);
+
+        TextView textView = findViewById(R.id.textView4);
+
+        Bundle arguments = getIntent().getExtras();
+        String flightnum = arguments.get("flightnum").toString();
+        text = "Рейс: " + flightnum;
+        textView.setText(text);
     }
 }
